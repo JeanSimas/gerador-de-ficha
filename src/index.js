68e4142630f265de855a9ca6d68b4154132eec73
Jean Simas <jeansimas12@hotmail.com>
2022-03-24 11:23:31 -0300

    Preparado para o Github

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const dateToSearch = process.argv[2]
 //extract date's problems from excel
 const validadores = parseExcelFile(dateToSearch)
 
+try {
+  fs.mkdirSync(path.join(__dirname, 'assets'))
+  fs.mkdirSync(path.join(__dirname, 'outDir'))
+  fs.mkdirSync(path.join(__dirname, 'temp'))
+} catch (error) {
+  
+}
 if(validadores.length === 0) console.log('Não existe registro nessa data');
 
 else {
@@ -24,20 +31,24 @@ else {
     
   }
 
-  (async () => {
-    validadores.forEach(validador => {
-      const tempHtml = createTempHtml(validador.data, validador.serialN, validador.falha, validador.fileName)
-      generatePdfFromHtmlPage(tempHtml, outDirPath, validador.fileName).then(() => {
-        console.log(`Arquivo ${validador.fileName} criado com êxito`);
-        deleteTempFile(tempHtml)
-  
+  try {
+    (async () => {
+      validadores.forEach(validador => {
+        const tempHtml = createTempHtml(validador.data, validador.serialN, validador.falha, validador.fileName)
+        generatePdfFromHtmlPage(tempHtml, outDirPath, validador.fileName).then(() => {
+          console.log(`Arquivo ${validador.fileName} criado com êxito`);
+          deleteTempFile(tempHtml)
+    
+        })
+    
       })
-  
+    })().then(() => {
+      require('child_process').exec(`start "" "${outDirPath}"`);
     })
-  })().then(() => {
-    require('child_process').exec(`start "" "${outDirPath}"`);
-  })
-
+  
+  } catch (error) {
+    console.log(error.toString())
+  }
 
 
 }      
